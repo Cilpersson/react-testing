1729ecfea4f64e19eaa223ae99a3c3487ffde223
Christina Persson <c.i.l.persson@gmail.com>
2020-12-03 19:46:08 +0100

    Removed snapshot

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -7,7 +7,9 @@ A snapshot, as the name suggests, allows us to save the snapshot of a given comp
 It helps a lot when you update or do some refactoring, and want to get or compare the changes.
 */
 
-it("should take a snapshot", () => {
-  const { asFragment } = render(<App />);
-  expect(asFragment(<App />)).toMatchSnapshot();
+describe("App", () => {
+  it("should take a snapshot", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment(<App />)).toMatchSnapshot();
+  });
 });
